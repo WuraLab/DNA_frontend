14475e367502893dd1d83a24e1676b5c0954406d
robogeek <azeezlukman95@gmail.com>
2020-07-11 02:33:06 +0100

    fixed bug

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { FolderPage } from "./folder.page";
+import { FolderComponent } from "./folder.page";
 
 const routes: Routes = [
   {
     path: "",
-    component: FolderPage
+    component: FolderComponent
   }
 ];
 
