2f6240951e28e7b08f532710815661244aa3128d
robogeek <azeezlukman95@gmail.com>
2020-07-21 19:26:02 +0100

    signup validation

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-signup",
@@ -6,10 +7,24 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./signup.page.scss"],
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    // eslint-disable-next-line max-len
+    const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.signupForm = this.fb.group({
+      name: ["", [Validators.required, Validators.minLength(3)]],
+      email: ["", [Validators.required, Validators.pattern(emailRe)]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  onSubmit(form: FormGroup) {
+    console.log(this.signupForm.invalid);
+    // console.log("Valid?", form.valid); // true or false
+    // console.log("email", form.value.email);
+    // console.log("password", form.value.password);
+  }
 }
