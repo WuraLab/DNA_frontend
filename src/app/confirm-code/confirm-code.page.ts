0ef13cf4af9b08715f319d9375fdec174a51c9da
Robogeeek <azeezlukman95@gmail.com>
2020-08-20 11:55:50 +0000

    completied ui for forgot password

--- src/app/confirm-code/confirm-code.page.ts
+++ src/app/confirm-code/confirm-code.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-confirm-code",
@@ -6,10 +7,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./confirm-code.page.scss"],
 })
 export class confirmCodeComponent implements OnInit {
-
-  constructor() { }
+  codeForm: FormGroup
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.codeForm = this.fb.group({
+      code: ["", [Validators.required]]
+    });
   }
 
 }
