86463f7153c14d79058628336a9b068cbeb90316
Azeez Lukman <azeezlukman95@gmail.com>
2020-09-05 18:47:51 +0100

    fixed linting

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -20,7 +20,7 @@ export class DashboardComponent implements OnInit {
     this.authState = this.authService.isAuthenticated();
     if (this.authState) {
       // this.storage.keys().then((info) => (this.userInfo = info));
-      this.storage.get("USER_INFO").then((info) => {
+      this.storage.get("USER_INFO").then(info => {
         this.userInfo = info;
       });
     }
